06c208577fe61e8f61955999814c73f342387f51
cilen <murilopiresandradecruz27@gmail.com>
2025-01-28 14:15:56 -0300

    starter

---
+++ src/test/java/com/murilopires/desafiotodolist/DesafiotodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilopires.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
